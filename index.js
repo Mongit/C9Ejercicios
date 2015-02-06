4da9599650a98849b9effd3234ca2706c9fba6ef
mongit <monserrat.jimenez2@gmail.com>
2015-02-06 00:45:55 +0000

    debuggeando con C9.io

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+const m = require("./modules/chapter3.js");
+
+m.countChar("Hola hay que hacer una cosa maravillosa", "a");
+process.exit(0);
---
+++ modules/chapter3.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let countChar = function(text, letter) {
+    
+    let count = 0;
+    
+    for(let i = 0; i < text.length; i++) {
+        
+        if (text.charAt(i) === letter) {
+            count++;
+        }
+    }
+    
+    return count;
+}
+
+exports.countChar = countChar;
